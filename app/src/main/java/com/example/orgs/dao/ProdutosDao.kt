bf2ad4f7ea13ebcedd43882f0913f559f4d9a87d
EnriqueIzel2 <eleaoizel@gmail.com>
2022-11-26 12:08:13 -0400

    feat: create companion object

--- app/src/main/java/com/example/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/com/example/orgs/dao/ProdutosDao.kt
@@ -3,7 +3,6 @@ package com.example.orgs.dao
 import com.example.orgs.model.Produto
 
 class ProdutosDao {
-  private val produtos = mutableListOf<Produto>()
 
   fun adiciona(produto: Produto) {
     produtos.add(produto)
@@ -11,5 +10,9 @@ class ProdutosDao {
 
   fun buscaTodos(): List<Produto> {
     return produtos.toList()
+  }
+
+  companion object {
+    private val produtos = mutableListOf<Produto>()
   }
 }
