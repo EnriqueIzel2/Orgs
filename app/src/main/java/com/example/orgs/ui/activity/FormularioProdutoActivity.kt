81f65b68cd2428857b0f1dc7e0ff9243421aa332
EnriqueIzel2 <eleaoizel@gmail.com>
2023-01-04 21:38:31 -0400

    feat: create form to load on preview and form

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -3,9 +3,10 @@ package com.example.orgs.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.example.orgs.R
+import coil.load
 import com.example.orgs.dao.ProdutosDao
 import com.example.orgs.databinding.ActivityFormularioProdutoBinding
+import com.example.orgs.databinding.FormularioImagemBinding
 import com.example.orgs.model.Produto
 import java.math.BigDecimal
 
@@ -21,9 +22,18 @@ class FormularioProdutoActivity : AppCompatActivity() {
     configuraBotaoSalvar()
 
     binding.activityFormularioProdutoImagem.setOnClickListener {
+      val bindingFormularioImagem = FormularioImagemBinding.inflate(layoutInflater)
+      bindingFormularioImagem.formularioImagemBotaoCarregar.setOnClickListener {
+        val url = bindingFormularioImagem.formularioImagemUrl.text.toString()
+        bindingFormularioImagem.formularioImagemImageView.load(url)
+      }
+
       AlertDialog.Builder(this)
-        .setView(R.layout.formulario_imagem)
-        .setPositiveButton("Confirmar") { _, _ -> }
+        .setView(bindingFormularioImagem.root)
+        .setPositiveButton("Confirmar") { _, _ ->
+          val url = bindingFormularioImagem.formularioImagemUrl.text.toString()
+          binding.activityFormularioProdutoImagem.load(url)
+        }
         .setNegativeButton("Cancelar") { _, _ -> }
         .show()
     }
