a9debeec9c0ea706649783af6e55c0b21d58bd83
enriqueizel2 <eleaoizel@gmail.com>
2023-01-05 17:02:24 -0400

    feat: add new attribute

--- app/src/main/java/com/example/orgs/model/Produto.kt
+++ app/src/main/java/com/example/orgs/model/Produto.kt
@@ -2,4 +2,9 @@ package com.example.orgs.model
 
 import java.math.BigDecimal
 
-data class Produto(val nome: String, val descricao: String, val valor: BigDecimal)
+data class Produto(
+  val nome: String,
+  val descricao: String,
+  val valor: BigDecimal,
+  val imagem: String? = null
+)
