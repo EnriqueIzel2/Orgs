3ba50c915432ec13c4c418f0cb1a197d8c363397
enriqueizel2 <eleaoizel@gmail.com>
2023-01-06 15:02:39 -0400

    feat: add imageLoader to load gif

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,8 +1,12 @@
 package com.example.orgs.ui.activity
 
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.load
 import com.example.orgs.dao.ProdutosDao
 import com.example.orgs.databinding.ActivityFormularioProdutoBinding
@@ -21,19 +25,27 @@ class FormularioProdutoActivity : AppCompatActivity() {
     setContentView(binding.root)
 
     configuraBotaoSalvar()
+    val imageLoader = ImageLoader.Builder(applicationContext)
+      .components {
+        if (SDK_INT >= 28) {
+          add(ImageDecoderDecoder.Factory())
+        } else {
+          add(GifDecoder.Factory())
+        }
+      }.build()
 
     binding.activityFormularioProdutoImagem.setOnClickListener {
       val bindingFormularioImagem = FormularioImagemBinding.inflate(layoutInflater)
       bindingFormularioImagem.formularioImagemBotaoCarregar.setOnClickListener {
         val url = bindingFormularioImagem.formularioImagemUrl.text.toString()
-        bindingFormularioImagem.formularioImagemImageView.load(url)
+        bindingFormularioImagem.formularioImagemImageView.load(url, imageLoader)
       }
 
       AlertDialog.Builder(this)
         .setView(bindingFormularioImagem.root)
         .setPositiveButton("Confirmar") { _, _ ->
           url = bindingFormularioImagem.formularioImagemUrl.text.toString()
-          binding.activityFormularioProdutoImagem.load(url)
+          binding.activityFormularioProdutoImagem.load(url, imageLoader)
         }
         .setNegativeButton("Cancelar") { _, _ -> }
         .show()
