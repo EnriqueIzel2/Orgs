8728fd5f766724734c24e4041ace30f1cad4d48d
EnriqueIzel2 <eleaoizel@gmail.com>
2023-01-11 21:41:29 -0400

    feat: create handler for ImagemDialog not resolve yet

---
+++ app/src/main/java/com/example/orgs/ui/dialog/FormularioImagemDialog.kt
@@ -0,0 +1,27 @@
+package com.example.orgs.ui.dialog
+
+import android.content.Context
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import com.example.orgs.databinding.FormularioImagemBinding
+import com.example.orgs.extensions.tentaCarregarImagem
+
+class FormularioImagemDialog(private val context: Context) {
+
+  fun mostra() {
+    val binding = FormularioImagemBinding.inflate(LayoutInflater.from(context))
+    binding.formularioImagemBotaoCarregar.setOnClickListener {
+      val url = binding.formularioImagemUrl.text.toString()
+      binding.formularioImagemImageView.tentaCarregarImagem(url)
+    }
+
+    AlertDialog.Builder(context)
+      .setView(binding.root)
+      .setPositiveButton("Confirmar") { _, _ ->
+        val url = binding.formularioImagemUrl.text.toString()
+//        binding.activityFormularioProdutoImagem.tentaCarregarImagem(url)
+      }
+      .setNegativeButton("Cancelar") { _, _ -> }
+      .show()
+  }
+}
