73ebd27c7da977819491232d71f765bf11e1d826
EnriqueIzel2 <eleaoizel@gmail.com>
2023-01-12 22:02:20 -0400

    refactor: implement behavior to get url

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.orgs.dao.ProdutosDao
 import com.example.orgs.databinding.ActivityFormularioProdutoBinding
+import com.example.orgs.extensions.tentaCarregarImagem
 import com.example.orgs.model.Produto
 import com.example.orgs.ui.dialog.FormularioImagemDialog
 import java.math.BigDecimal
@@ -21,7 +22,10 @@ class FormularioProdutoActivity : AppCompatActivity() {
     configuraBotaoSalvar()
 
     binding.activityFormularioProdutoImagem.setOnClickListener {
-      FormularioImagemDialog(this).mostra()
+      FormularioImagemDialog(this).mostra { it ->
+        url = it
+        binding.activityFormularioProdutoImagem.tentaCarregarImagem(url)
+      }
     }
   }
 
