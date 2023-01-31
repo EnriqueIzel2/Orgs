6a8dc244ae914dacc6379532970a466a028a224c
enriqueizel2 <eleaoizel@gmail.com>
2023-01-31 17:45:21 -0400

    feat: create function to convert value from Double to BigDecimal

---
+++ app/src/main/java/com/example/orgs/database/converter/Converters.kt
@@ -0,0 +1,12 @@
+package com.example.orgs.database.converter
+
+import androidx.room.TypeConverter
+import java.math.BigDecimal
+
+class Converters {
+
+  @TypeConverter
+  fun deDouble(valor: Double?): BigDecimal {
+    return valor?.let { BigDecimal(valor.toString()) } ?: BigDecimal.ZERO
+  }
+}
