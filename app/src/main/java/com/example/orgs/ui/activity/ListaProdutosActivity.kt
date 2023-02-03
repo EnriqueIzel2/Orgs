13b73794fc89d6f5f4167018568d94f17f178608
enriqueizel2 <eleaoizel@gmail.com>
2023-02-03 11:18:25 -0400

    feat: add connection with database

--- app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -5,10 +5,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.example.orgs.dao.ProdutosDao
+import com.example.orgs.database.AppDatabase
 import com.example.orgs.databinding.ActivityListaProdutosBinding
+import com.example.orgs.model.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.math.BigDecimal
 
 class ListaProdutosActivity : AppCompatActivity() {
   private val dao = ProdutosDao()
@@ -23,6 +27,21 @@ class ListaProdutosActivity : AppCompatActivity() {
 
     configuraRecyclerView()
     configuraFab()
+
+    val db = Room.databaseBuilder(
+      this,
+      AppDatabase::class.java,
+      "orgs.db"
+    ).allowMainThreadQueries().build()
+
+    val produtoDao = db.produtoDao()
+    produtoDao.salva(
+      Produto(
+        nome = "teste nome 1",
+        descricao = "teste desc 1",
+        valor = BigDecimal("10.51")
+      )
+    )
   }
 
   override fun onResume() {
