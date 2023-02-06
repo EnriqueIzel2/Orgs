0db34f1d42470c9100802ef2ea3b24d24e566def
enriqueizel2 <eleaoizel@gmail.com>
2023-02-06 17:14:57 -0400

    feat: create function to get instance from AppDatabase

--- app/src/main/java/com/example/orgs/database/AppDatabase.kt
+++ app/src/main/java/com/example/orgs/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.orgs.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.orgs.database.converter.Converters
@@ -11,4 +13,14 @@ import com.example.orgs.model.Produto
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
   abstract fun produtoDao(): ProdutoDao
+
+  companion object {
+    fun instancia(context: Context): AppDatabase {
+      return Room.databaseBuilder(
+        context,
+        AppDatabase::class.java,
+        "orgs.db"
+      ).allowMainThreadQueries().build()
+    }
+  }
 }
