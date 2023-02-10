8ea17fdab0fb3be1354af69aafdd9ad05a311452
enriqueizel2 <eleaoizel@gmail.com>
2023-02-10 17:19:24 -0400

    feat: add function onOptionsItemSelected

--- app/src/main/java/com/example/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -1,13 +1,17 @@
 package com.example.orgs.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.orgs.R
 import com.example.orgs.databinding.ActivityDetalhesProdutoBinding
 import com.example.orgs.extensions.formatarMoedaBrasileira
 import com.example.orgs.extensions.tentaCarregarImagem
 import com.example.orgs.model.Produto
+
+private const val TAG = "Detalhes Produto"
 
 class DetalhesProdutoActivity : AppCompatActivity() {
 
@@ -25,6 +29,18 @@ class DetalhesProdutoActivity : AppCompatActivity() {
   override fun onCreateOptionsMenu(menu: Menu?): Boolean {
     menuInflater.inflate(R.menu.detalhes_produto_menu, menu)
     return super.onCreateOptionsMenu(menu)
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      R.id.menu_detalhes_produto_remover -> {
+        Log.i(TAG, "onOptionsItemSelected: remover")
+      }
+      R.id.menu_detalhes_produto_editar -> {
+        Log.i(TAG, "onOptionsItemSelected: editar")
+      }
+    }
+    return super.onOptionsItemSelected(item)
   }
 
   private fun tentaCarregarProduto() {
