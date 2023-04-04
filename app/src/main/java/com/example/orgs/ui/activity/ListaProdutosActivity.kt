cf423035f3751e78b64c03c602ba717486510f1e
EnriqueIzel2 <eleaoizel@gmail.com>
2023-04-04 18:44:32 -0400

    refactor: use best practices

--- app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -7,15 +7,23 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.database.AppDatabase
+import com.example.orgs.database.dao.ProdutoDao
 import com.example.orgs.databinding.ActivityListaProdutosBinding
+import com.example.orgs.model.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.*
 
 class ListaProdutosActivity : AppCompatActivity() {
+  private val TAG = "ListaProdutosActivity"
   private val adapter = ListaProdutosAdapter(context = this)
   private val binding by lazy {
     ActivityListaProdutosBinding.inflate(layoutInflater)
+  }
+  private val job = Job()
+  private val dao by lazy {
+    val db = AppDatabase.instancia(this)
+    db.produtoDao()
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,25 +36,30 @@ class ListaProdutosActivity : AppCompatActivity() {
 
   override fun onResume() {
     super.onResume()
-    val db = AppDatabase.instancia(this)
-    val produtoDao = db.produtoDao()
-    val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
-      Toast.makeText(this@ListaProdutosActivity, "Deu ruim", Toast.LENGTH_SHORT).show()
-    }
+    val scope = MainScope()
 
-    val mainScope = MainScope()
+//    scope.launch(job + handler + Dispatchers.IO + CoroutineName("primerio")) {
+//      repeat(1000) {
+//        Log.i(TAG, "onResume: coroutine está em execução $it")
+//        delay(1000)
+//      }
+//    }
 
-    mainScope.launch(handler) {
-      MainScope().launch(handler) {
-        throw Exception("Em outro escopo")
-      }
-      throw Exception("Lançando na coroutine")
-      val produtos = withContext(Dispatchers.IO) {
-        produtoDao.buscarTodos()
-      }
+    scope.launch {
+      val produtos = buscaTodosProdutos()
       adapter.atualiza(produtos)
     }
   }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    job.cancel()
+  }
+
+  private suspend fun buscaTodosProdutos() =
+    withContext(Dispatchers.IO) {
+      dao.buscarTodos()
+    }
 
   private fun configuraFab() {
     val fab: FloatingActionButton = binding.floatingActionButton
