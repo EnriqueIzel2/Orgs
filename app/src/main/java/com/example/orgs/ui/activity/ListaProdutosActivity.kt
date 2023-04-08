ccef794e4320dc49a659d425ea0515d0905c7b85
EnriqueIzel2 <eleaoizel@gmail.com>
2023-04-08 19:29:26 -0400

    feat: remove unused functions

--- app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ListaProdutosActivity.kt
@@ -3,16 +3,15 @@ package com.example.orgs.ui.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.database.AppDatabase
-import com.example.orgs.database.dao.ProdutoDao
 import com.example.orgs.databinding.ActivityListaProdutosBinding
-import com.example.orgs.model.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.coroutines.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ListaProdutosActivity : AppCompatActivity() {
   private val TAG = "ListaProdutosActivity"
@@ -20,7 +19,6 @@ class ListaProdutosActivity : AppCompatActivity() {
   private val binding by lazy {
     ActivityListaProdutosBinding.inflate(layoutInflater)
   }
-  private val job = Job()
   private val dao by lazy {
     val db = AppDatabase.instancia(this)
     db.produtoDao()
@@ -36,30 +34,19 @@ class ListaProdutosActivity : AppCompatActivity() {
 
   override fun onResume() {
     super.onResume()
-    val scope = MainScope()
 
-//    scope.launch(job + handler + Dispatchers.IO + CoroutineName("primerio")) {
-//      repeat(1000) {
-//        Log.i(TAG, "onResume: coroutine está em execução $it")
-//        delay(1000)
-//      }
-//    }
+    lifecycleScope.launch {
+      repeat(1000) {
+        Log.i(TAG, "onResume: coroutine está em execução $it")
+        delay(1000)
+      }
+    }
 
-    scope.launch {
-      val produtos = buscaTodosProdutos()
+    lifecycleScope.launch {
+      val produtos = dao.buscarTodos()
       adapter.atualiza(produtos)
     }
   }
-
-  override fun onDestroy() {
-    super.onDestroy()
-    job.cancel()
-  }
-
-  private suspend fun buscaTodosProdutos() =
-    withContext(Dispatchers.IO) {
-      dao.buscarTodos()
-    }
 
   private fun configuraFab() {
     val fab: FloatingActionButton = binding.floatingActionButton
