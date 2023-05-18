3e71221ce29d07e64a624ee78997eb5bd0f4a84e
EnriqueIzel2 <eleaoizel@gmail.com>
2023-05-18 19:32:13 -0400

    feat: add query to authenticate user

--- app/src/main/java/com/example/orgs/database/dao/UsuarioDao.kt
+++ app/src/main/java/com/example/orgs/database/dao/UsuarioDao.kt
@@ -2,6 +2,7 @@ package com.example.orgs.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.orgs.model.Usuario
 
 @Dao
@@ -9,4 +10,7 @@ interface UsuarioDao {
 
   @Insert
   suspend fun salva(usuario: Usuario)
+
+  @Query("SELECT * FROM Usuario WHERE id = :usuarioID AND senha = :senha")
+  suspend fun autentica(usuarioID: String, senha: String): Usuario?
 }
