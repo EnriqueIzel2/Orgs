a5796816db7e7f9c33734a077faf26535d9af894
EnriqueIzel2 <eleaoizel@gmail.com>
2023-05-18 19:32:35 -0400

    feat: add authentication on login

--- app/src/main/java/com/example/orgs/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/LoginActivity.kt
@@ -2,14 +2,22 @@ package com.example.orgs.ui.activity
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.orgs.database.AppDatabase
 import com.example.orgs.databinding.ActivityLoginBinding
 import com.example.orgs.extensions.vaiPara
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
 
   private val binding by lazy {
     ActivityLoginBinding.inflate(layoutInflater)
+  }
+
+  private val usuarioDao by lazy {
+    AppDatabase.instancia(this).usuarioDao()
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +33,15 @@ class LoginActivity : AppCompatActivity() {
       val usuario = binding.activityLoginUsuario.text.toString()
       val senha = binding.activityLoginSenha.text.toString()
       Log.i("LoginActivity", "onCreate: $usuario - $senha")
-      vaiPara(ListaProdutosActivity::class.java)
+      lifecycleScope.launch {
+        usuarioDao.autentica(usuario, senha)?.let {
+          vaiPara(ListaProdutosActivity::class.java)
+        } ?: Toast.makeText(
+          this@LoginActivity,
+          "Falha na autenticação",
+          Toast.LENGTH_SHORT
+        ).show()
+      }
     }
   }
 
