012d962b50e5d796e6797df769e3b184cad4dfbf
EnriqueIzel2 <eleaoizel@gmail.com>
2023-05-18 19:59:13 -0400

    feat: add query for user by ID

--- app/src/main/java/com/example/orgs/database/dao/UsuarioDao.kt
+++ app/src/main/java/com/example/orgs/database/dao/UsuarioDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.orgs.model.Usuario
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UsuarioDao {
@@ -13,4 +14,7 @@ interface UsuarioDao {
 
   @Query("SELECT * FROM Usuario WHERE id = :usuarioID AND senha = :senha")
   suspend fun autentica(usuarioID: String, senha: String): Usuario?
+
+  @Query("SELECT * FROM Usuario WHERE id = :usuarioID")
+  fun buscaPorID(usuarioID: String): Flow<Usuario>
 }
