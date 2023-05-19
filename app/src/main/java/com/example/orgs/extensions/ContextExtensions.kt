955acd25f42e238883c85ec15be50487faf2640b
EnriqueIzel2 <eleaoizel@gmail.com>
2023-05-18 20:01:04 -0400

    feat: add callback function to put extra

--- app/src/main/java/com/example/orgs/extensions/ContextExtensions.kt
+++ app/src/main/java/com/example/orgs/extensions/ContextExtensions.kt
@@ -3,8 +3,9 @@ package com.example.orgs.extensions
 import android.content.Context
 import android.content.Intent
 
-fun Context.vaiPara(clazz: Class<*>) {
+fun Context.vaiPara(clazz: Class<*>, intent: Intent.() -> Unit = {}) {
   Intent(this, clazz).apply {
+    intent()
     startActivity(this)
   }
 }
