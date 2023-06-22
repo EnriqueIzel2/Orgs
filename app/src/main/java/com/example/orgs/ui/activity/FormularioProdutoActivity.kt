ebba0e55d172055c26de6eec078bffb04eb03bc3
EnriqueIzel2 <eleaoizel@gmail.com>
2023-06-22 19:51:39 -0400

    refactor: remove duplicated functions

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -1,28 +1,22 @@
 package com.example.orgs.ui.activity
 
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.orgs.database.AppDatabase
 import com.example.orgs.databinding.ActivityFormularioProdutoBinding
 import com.example.orgs.extensions.tentaCarregarImagem
 import com.example.orgs.model.Produto
-import com.example.orgs.preferences.dataStore
-import com.example.orgs.preferences.usuarioLogadoPreferences
 import com.example.orgs.ui.dialog.FormularioImagemDialog
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 import java.math.BigDecimal
 
-class FormularioProdutoActivity : AppCompatActivity() {
+class FormularioProdutoActivity : UsuarioBaseActivity() {
   private val binding by lazy {
     ActivityFormularioProdutoBinding.inflate(layoutInflater)
   }
   private val produtoDao by lazy {
     AppDatabase.instancia(this).produtoDao()
-  }
-  private val usuarioDao by lazy {
-    AppDatabase.instancia(this).usuarioDao()
   }
 
   private var url: String? = null
@@ -46,12 +40,8 @@ class FormularioProdutoActivity : AppCompatActivity() {
     tentaBuscarProduto()
 
     lifecycleScope.launch {
-      dataStore.data.collect { preferences ->
-        preferences[usuarioLogadoPreferences]?.let { usuarioID ->
-          usuarioDao.buscaPorID(usuarioID).collect {
-            Log.i("Formulario Produtos", "onCreate: $it")
-          }
-        }
+      usuario.filterNotNull().collect {
+
       }
     }
   }
